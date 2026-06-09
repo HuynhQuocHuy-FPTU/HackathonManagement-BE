2cb4cb6453ef1c55893694b36ae49b6b8f1a245b
VoThiThuyNgan-05 <thungancm2005@gmail.com>
2026-06-09 16:07:57 +0700

    delete service

--- SWP_Hackathon/src/main/java/com/hackathon/service/EvaluationCriteriaService.java
+++ SWP_Hackathon/src/main/java/com/hackathon/service/EvaluationCriteriaService.java
@@ -6,19 +6,19 @@ import com.hackathon.dto.criteria.request.EvaluationCriteriaRequestDTO;
 import java.util.List;
 
 public interface EvaluationCriteriaService {
-    List<EvaluationCriteriaRequestDTO> getEvaluationCriteriaByCriteriaId(Integer criteriaId);
-
-    EvaluationCriteriaResponseDTO saveEvaluationCriteria(EvaluationCriteriaRequestDTO evaluationCriteriaRequestDTO);
-
-    // Lấy theo eventId — biết event này đang dùng bộ tiêu chí nào
-    EvaluationCriteriaResponseDTO getByEventId(Integer eventId);
-
-    //Update
-    void updateEvaluationCriteria(EvaluationCriteriaRequestDTO evaluationCriteriaRequestDTO);
-
-    //Delete
-    void deleteEvaluationCriteriaById(Integer id);
-
-    //Delete 1 item
-    void deleteAllCriteriaByRoundId(Integer id);
+//    List<EvaluationCriteriaRequestDTO> getEvaluationCriteriaByCriteriaId(Integer criteriaId);
+//
+//    EvaluationCriteriaResponseDTO saveEvaluationCriteria(EvaluationCriteriaRequestDTO evaluationCriteriaRequestDTO);
+//
+//    // Lấy theo eventId — biết event này đang dùng bộ tiêu chí nào
+//    EvaluationCriteriaResponseDTO getByEventId(Integer eventId);
+//
+//    //Update
+//    void updateEvaluationCriteria(EvaluationCriteriaRequestDTO evaluationCriteriaRequestDTO);
+//
+//    //Delete
+//    void deleteEvaluationCriteriaById(Integer id);
+//
+//    //Delete 1 item
+//    void deleteAllCriteriaByRoundId(Integer id);
 }
