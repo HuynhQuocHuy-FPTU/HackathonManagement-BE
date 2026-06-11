ec9cd56427a1bb801765a883478d45140ab147e5
MSI\saita <saitama0102huy@gmail.com>
2026-06-11 16:10:05 +0700

    Delete:file idea Update: localhost

--- SWP_Hackathon/src/main/java/com/hackathon/config/SecurityConfig.java
+++ SWP_Hackathon/src/main/java/com/hackathon/config/SecurityConfig.java
@@ -38,9 +38,12 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/account/**",
+                                "/api/**",
                                 "/error",
-                                "/verify-email"
+                                "/verify-email",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
