1fbe7a83a6ba133e18b853df02cc96cb62e1fd7b
hothuydiep <hodiep4725@gmail.com>
2026-06-20 18:45:20 +0700

    feat: Event coordinator approve/reject registration and assign category and team

--- SWP_Hackathon/src/main/java/com/hackathon/config/SecurityConfig.java
+++ SWP_Hackathon/src/main/java/com/hackathon/config/SecurityConfig.java
@@ -45,20 +45,6 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         //các API công khai ai cũng vào được
                         .requestMatchers(
-//                                "/api/account/**",   // N Them de test
-//                                "/api/account/login",      //  N them de test
-//                                "/api/account/resend-verification", //N them
-//                                "/api/notifications/**",
-//                                "/api/events",//N
-//                                "/api/events/*",//N
-//                                "/error",
-//                                "/verify-email",
-//                                "/swagger-ui/**",
-//                                "/swagger-ui.html",
-//                                "/v3/api-docs/**",
-//                                "/api/events/public/**",
-//                                "/api/events/*/detail",
-//                                "/api/events/all"
                                 "/api/account/**",
                                 "/api/notifications/**",
                                 "/api/events/all",
@@ -67,19 +53,6 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**"
                         ).permitAll()
-//                        //CriteriaSet chỉ có Coordinator là người có quyền truy cập
-//                        .requestMatchers("/api/criteriaSet/**")
-//                        .hasRole("EVENTCOORDINATOR")
-//                        .requestMatchers("/api/notifications/**").authenticated()
-//                        .requestMatchers("/api/events/registration/**").hasRole("STUDENT")
-//                        //Chỉ event coordinator mới có quyền tạo event
-//                        .requestMatchers(HttpMethod.POST, "/api/events").hasRole("EVENTCOORDINATOR")
-//                        .requestMatchers(HttpMethod.PUT, "/api/events/**").hasRole("EVENTCOORDINATOR")
-//                        .requestMatchers(HttpMethod.DELETE, "/api/events/**").hasRole("EVENTCOORDINATOR")
-//                        .requestMatchers(HttpMethod.PATCH, "/api/events/**").hasRole("EVENTCOORDINATOR")
-//                        //admin
-//                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
-//                        .anyRequest().authenticated()
                                 .requestMatchers("/api/criteriaSet/**").hasRole("EVENTCOORDINATOR")
                                 .requestMatchers(HttpMethod.PATCH, "/api/events/*/approve", "/api/events/*/reject").hasRole("EVENTCOORDINATOR")
                                 .requestMatchers(HttpMethod.GET, "/api/events/*/approved-teams").hasRole("EVENTCOORDINATOR")
