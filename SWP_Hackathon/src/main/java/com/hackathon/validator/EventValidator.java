3c4ba8c183090719036f72652e2fe819387a87d6
MSI\saita <saitama0102huy@gmail.com>
2026-08-03 17:12:00 +0700

    feat: check email github matchs with email account

--- SWP_Hackathon/src/main/java/com/hackathon/validator/EventValidator.java
+++ SWP_Hackathon/src/main/java/com/hackathon/validator/EventValidator.java
@@ -88,20 +88,20 @@ public class EventValidator {
                 throw new BadRequestException("Hạn chót đăng ký không được nằm trong quá khứ!");
 
             if(deadline.isAfter(start)){
-                throw new BadRequestException("Hạn chót đăng ký phải trước ngày bắt đầu diễn ra buổi workshop");
+                throw new BadRequestException("Hạn chót đăng ký phải trước ngày bắt đầu diễn ra sự kiện");
             }
         }
 
         if (workshop != null) {
             if (validatePast && workshop.isBefore(now))
-                throw new BadRequestException("Workshop không được nằm trong quá khứ!");
+                throw new BadRequestException("buổi bóc thăm không được nằm trong quá khứ!");
 
             if(deadline != null && workshop.isBefore(deadline)){
-                throw new BadRequestException("Ngày diễn ra workshop không được bắt đầu trước ngày kết thúc đăng kí tham gia");
+                throw new BadRequestException("Ngày diễn ra bóc thăm không được bắt đầu trước ngày kết thúc đăng kí tham gia");
             }
 
             if (start != null && workshop.isAfter(start)) {
-                throw new BadRequestException("Workshop phải trước ngày bắt đầu sự kiện");
+                throw new BadRequestException("buổi bóc thăm phải trước ngày bắt đầu sự kiện");
             }
 
         }
