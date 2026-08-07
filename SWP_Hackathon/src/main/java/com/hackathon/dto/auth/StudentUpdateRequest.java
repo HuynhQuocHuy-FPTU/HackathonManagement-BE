3c25fb78547febdc5a00fa8fe2bbe5d30747dd24
Trần Quốc Thịnh <tranquocthinh0124@gmail.com>
2026-08-07 20:50:18 +0700

    refactor(dto): update validation constraints for registration and update requests

--- SWP_Hackathon/src/main/java/com/hackathon/dto/auth/StudentUpdateRequest.java
+++ SWP_Hackathon/src/main/java/com/hackathon/dto/auth/StudentUpdateRequest.java
@@ -9,16 +9,19 @@ import lombok.Setter;
 @Getter
 @Setter
 public class StudentUpdateRequest {
+
     @NotBlank(message = "Số điện thoại không được để trống")
-    @Pattern(regexp = "^[0-9]{10}$", message = "Số điện thoại phải gồm đúng 10 chữ số")
+    @Pattern(regexp = "^(0)[0-9]{9}$", message = "Số điện thoại không hợp lệ (Phải bắt đầu bằng 0 hoặc +84 và theo sau là 9 chữ số)")
     private String phone;
 
     @NotBlank(message = "Mã số sinh viên không được để trống")
     @Size(max = 20, message = "Mã số sinh viên tối đa 20 ký tự")
+    @Pattern(regexp = "^0[0-9]{9}$", message = "Mã số sinh viên chỉ được chứa chữ cái và số")
     private String studentCode;
 
     @NotBlank(message = "Tên sinh viên không được để trống")
     @Size(max = 50, message = "Tên sinh viên tối đa 50 ký tự")
+    @Pattern(regexp = "^[\\p{L}\\s]+$", message = "Tên sinh viên chỉ được chứa chữ cái và khoảng trắng")
     private String studentName;
 
     @NotBlank(message = "Địa chỉ không được để trống")
@@ -30,7 +33,8 @@ public class StudentUpdateRequest {
     private String major;
 
     @NotBlank(message = "University không được để trống")
-    @Size(max = 255, message = "University tối đa 255 ký tự")
+    @Size(max = 255, message = "Tên trường đại học tối đa 255 ký tự")
     private String university;
+
     private String avatar;
 }
